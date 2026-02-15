3374ab3ac39c8a8068f1c551434ea9b5cd83ec53
Analice <analicedantas07@gmail.com>
2026-02-15 15:49:41 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Projeto SaaS!');
+  res.end('Projeto IREDE!');
 });
 
 server.listen(port, hostname, () => {
